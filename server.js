8499c66c0ec8c11e581da656420a5c5a4efa869c
hackersgod2006 <masmat170290@gmail.com>
2026-05-12 13:05:10 +0100

    fix port

--- server.js
+++ server.js
@@ -37,4 +37,4 @@ Reply ONLY in this JSON format:
   }
 });
 
-app.listen(3000, () => console.log('ScamShield running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('ScamShield running on port process.env.PORT || 3000'));
